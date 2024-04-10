d0d63040e05f91035604d893426421fea04629f4
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2024-04-10 18:21:05 +0100

    frontend/src/pages/Shop/ShopCreateCategory.jsx

---
+++ frontend/src/pages/Shop/ShopCreateCategory.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import DashboardHeader from '../../components/Shop/Layout/DashboardHeader';
+import DashboardSideBar from '../../components/Shop/Layout/DashboardSideBar';
+import CreateCategory from '../../components/Shop/CreateShopCategory';
+
+const ShopCreateCategory = () => {
+
+  return (
+    <div>
+      <DashboardHeader />
+      <div className="flex items-center justify-between w-full">
+        <div className="w-[80px] 800px:w-[330px]">
+          <DashboardSideBar active={12} />
+        </div>
+        <div className="w-full justify-center flex">
+          <CreateCategory />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShopCreateCategory;
