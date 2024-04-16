c5e0591d7e8885ba6763f18f537062c89fc66769
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2024-04-16 07:21:06 +0100

    components/Route/NewArrivals/

---
+++ frontend/src/components/Route/NewArrivals/NewArrivals.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import styles from "../../../styles/styles";
+import ProductCard from "../ProductCard/ProductCard";
+
+const NewArrivals = () => {
+  const [data, setData] = useState([]);
+  const { allProducts } = useSelector((state) => state.products);
+  useEffect(() => {
+    const allProductsData = allProducts ? [...allProducts] : [];
+    const sortedData = allProductsData?.sort((a, b) => b.sold_out - a.sold_out);
+    const firstFive = sortedData && sortedData.slice(0, 6);
+    setData(firstFive);
+  }, [allProducts]);
+
+  return (
+    <div className="pb-10">
+      <div className="w-full flex flex-col mx-auto py-auto bg-white">
+        <div className={`${styles.heading}`}>
+          <h1>New Arrivals</h1>
+        </div>
+
+        <div className="flex overflow-x-scroll pb-16 hide-scroll-bar ">
+          {data && data.length !== 0 && (
+            <div className="flex gap-5">
+              {data.map((product, index) => (
+                <div
+                  key={index}
+                  className=" sm:w-40 md:w-60 lg:w-60 h-80  whitespace-normal"
+                >
+                  <ProductCard data={product} key={index} />
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewArrivals;
